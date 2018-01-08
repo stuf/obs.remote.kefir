3b016c39454b8cc3736ff1f1406f14c146d5be5e
Stefan Rimaila <stefan@rimaila.fi>
2018-01-08 17:33:32 +0200

    🔮 Add getEvents/Requests tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,6 +1,25 @@
 const util = require('../lib/util');
+const { Events, Requests } = require('../lib/api');
+
+const getRandomElem = xs => xs[Math.floor(Math.random() * xs.length)];
 
 describe('util', () => {
+  test(util.getEvent.toString(), () => {
+    const testCount = 25;
+    for (let i = 0; i < testCount; i++) {
+      const el = getRandomElem(Events);
+      expect(util.getEvent(el)).toBe(el);
+    }
+  });
+
+  test(util.getRequest.toString(), () => {
+    const testCount = 25;
+    for (let i = 0; i < testCount; i++) {
+      const el = getRandomElem(Requests);
+      expect(util.getRequest(el)).toBe(el);
+    }
+  });
+
   [[util.camelCasePascal, 'FooBar', 'fooBar'],
    [util.constCasePascal, 'FooBar', 'FOO_BAR'],
    [util.kebabCasePascal, 'FooBar', 'foo-bar'],
